90ac003c1e3ea74021c6309cb1ea86a9c8b38104
Felipe <felipepirovano95@gmail.com>
2021-11-11 14:26:04 -0300

    customizações e font

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: var(--blue);
-`
-
 export function App() {
   return (
     <div>
       <GlobalStyle/>
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
     </div>
   );
 } 
