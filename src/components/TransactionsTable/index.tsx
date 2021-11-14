0a638e164d413af682ccc9a6618de24f5458e1ae
Felipe <felipepirovano95@gmail.com>
2021-11-14 10:02:17 -0300

    transactions table add

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,5 +1,26 @@
-export function TransactionsTable(){
-  return(
-    <h1>Table</h1>
+import { Container } from './styles'
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
   )
 }
